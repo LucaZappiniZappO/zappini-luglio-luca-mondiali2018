1ea80ea52cfc82eda9073488936a973a1686f6ad
ZappO <luca.zappini-1@studenti.unitn.it>
2018-07-11 11:15:02 +0200

    getandpostteams

--- app.js
+++ app.js
@@ -4,17 +4,59 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+var teams =
+    [{
+        "id": 0, "name": "croatia", "is_still_in": true, "matches": [{ "id": 0, "opponent": 1, "outcome": 'W' }]
+    }, {
+        "id": 1, "name": "russia", "is_still_in": false, "matches": [{ "id": 0, "opponent": 0, "outcome": 'L' }]
+    }];
+
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/count', function (req, res) {
+app.get('/teams', function (req, res) {
+    res.json(teams);
+});
+
+app.get('/matches', function (req, res) {
+    var help = [];
+    for (var i = 0; i < teams.length; i++)
+        help.push(teams[i].matches);
+    res.json(help);
+});
+
+
+app.post('/teams', function (req, res) {
+    console.log(req.body);
+    if (req.body != undefined) {
+        if (req.body.name != undefined && req.body.is_still_in != undefined && req.body.matches != undefined && req.body.matches != []) {
+            req.body.id = teams.length;
+            teams.push(req.body);
+
+        }
+        else {
+            if (req.body.name != undefined && req.body.is_still_in != undefined) {
+                req.body.id = teams.length;
+                req.body.matches = [];
+                teams.push(req.body);
+            }
+        }
+    }
+    else
+        res.json({ "error": "missing body" });
+
+        res.json(teams);
+    
+});
+
+app.put('/teams', function (req, res) {
 
 });
 
-app.get('/sum', function (req, res) {
+app.post('/matches', function (req, res) {
 
 });
 
-app.post('/check', function (req, res) {
+app.put('/matches', function (req, res) {
 
 });
 
@@ -22,19 +64,3 @@ app.post('/check', function (req, res) {
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
 });
-
-function validInt(value) {
-    if (arguments.length !== 1)
-        return false;
-    if (value instanceof Array)
-        return false;
-    if (value === Infinity || value === -Infinity)
-        return true;
-    if (isNaN(value))
-        return false;
-    if (isNaN(parseInt(value)))
-        return false;
-    if (parseInt(Number(value)) !== value)
-        return false;
-    return true;
-}
