6e0c86ff278656a6565e6ba3da6570f6dc00b585
ZappO <luca.zappini-1@studenti.unitn.it>
2018-07-11 11:43:07 +0200

    putandpost

--- app.js
+++ app.js
@@ -17,16 +17,19 @@ app.get('/teams', function (req, res) {
     res.json(teams);
 });
 
-app.get('/matches', function (req, res) {
+function computeMatches() {
     var help = [];
     for (var i = 0; i < teams.length; i++)
         help.push(teams[i].matches);
-    res.json(help);
+    return help;
+}
+
+app.get('/matches', function (req, res) {
+    res.json(computeMatches());
 });
 
 
 app.post('/teams', function (req, res) {
-    console.log(req.body);
     if (req.body != undefined) {
         if (req.body.name != undefined && req.body.is_still_in != undefined && req.body.matches != undefined && req.body.matches != []) {
             req.body.id = teams.length;
@@ -44,20 +47,62 @@ app.post('/teams', function (req, res) {
     else
         res.json({ "error": "missing body" });
 
-        res.json(teams);
-    
+    res.json(teams);
+
 });
 
 app.put('/teams', function (req, res) {
-
+    if (req.body != undefined) {
+        if (req.body.id != undefined) {
+            for (var i = 0; i < teams.length; i++) {
+                if (teams[i].id == req.body.id) {
+                    teams[i].matches[i] = req.body;
+                }
+            }
+        }
+    }
+    else
+        res.json({ "error": "BODY ERROR!" });
+    res.json(teams);
 });
 
 app.post('/matches', function (req, res) {
+    if (req.body != undefined) {
+        if (req.body.idTeam != undefined && req.body.opponent != undefined && req.body.outcome != undefined && (req.body.outcome == 'W' || req.body.outcome == 'D' || req.body.outcome == 'L')) {
 
+            for (var i = 0; i < teams.length; i++) {
+                if (teams[i].id == req.body.idTeam) {
+                    var match = { "id": teams[i].matches.length, "opponent": req.body.opponent, "outcome": req.body.outcome }
+                    teams[i].matches.push(match);
+                }
+            }
+        }
+        else {
+            return res.json({ "error": "ERROR!" });
+        }
+    }
+    else
+        return res.json({ "error": "BODY ERROR!" });
+
+    return res.json(computeMatches());
 });
 
 app.put('/matches', function (req, res) {
+    if (req.body != undefined) {
+        if (req.body.id != undefined) {
+            for (var i = 0; i < teams.length; i++) {
+                for (var j = 0; j < teams[i].matches.length; j++) {
+                    if (teams[i].matches[j].id == req.body.id) {
+                        teams[i].matches[i] = req.body;
+                    }
+                }
+            }
+        }
+    }
+    else
+        res.json({ "error": "BODY ERROR!" });
 
+    res.json(computeMatches());
 });
 
 
